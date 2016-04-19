7447b3fb76f4a3241da280f8851a0b5d7262a0ba
Eva <eva.rolfes@gmail.com>
2016-04-19 18:22:07 +0200

    improved board

--- src/Board.java
+++ src/Board.java
@@ -9,14 +9,16 @@ public class Board {
     static final String PRINT_FIELD_FORMAT = "%02d";
     static final String PRINT_NEW_LINE = "\n";
 
-    static final int INVALID_FIELD = 0;
     static final int EMPTY_FIELD = -1;
 
-    final int[][] fields = new int[8][15];
+    final int[][] fields = new int[8][];
 
     public Board() {
         for (int y = fields.length - 1; y >= 0; y--) {
-            for (int x = 0; x < y * 2 + 1; x++) {
+            int rowLength = y * 2 + 1;
+            fields[y] = new int[rowLength];
+
+            for (int x = 0; x < rowLength; x++) {
                 fields[y][x] = EMPTY_FIELD;
             }
         }
@@ -27,17 +29,15 @@ public class Board {
         StringBuilder builder = new StringBuilder();
         StringBuilder header = new StringBuilder(PRINT_ORIGIN);
 
-        for (int y = fields.length - 1; y >= 0; y--) {
+        final int yMaxLength = fields.length - 1;
+        for (int y = yMaxLength; y >= 0; y--) {
             int[] row = fields[y];
             builder.append(y + PRINT_ROW_DELIMITER);
             for (int x = 0; x < row.length; x++) {
                 int field = row[x];
-                if (y == 0) {
+                if (y == yMaxLength) {
                     String xFormatted = String.format(PRINT_FIELD_FORMAT, x);
                     header.append(xFormatted).append(PRINT_SPACE);
-                }
-                if (INVALID_FIELD == field) {
-                    continue;
                 }
                 String fieldFormatted = String.format(PRINT_FIELD_FORMAT, field);
                 builder.append(fieldFormatted).append(PRINT_SPACE);
