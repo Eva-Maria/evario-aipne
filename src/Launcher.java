733d8c5184e4d84b3eca7ed7aaef6844fa95f75a
Mario Neises <git@mnio.net>
2016-05-16 13:49:02 +0200

    wrap system out to auto exit

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,8 @@
 import lenz.htw.aipne.Server;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by eve on 4/11/16.
@@ -18,6 +20,8 @@ public class Launcher {
 
     public static void main(String... args) throws IOException {
         String hostName = "127.0.0.1";
+
+        wrapSystemOut();
 
         if (args.length >= 1) {
             hostName = args[0];
@@ -41,5 +45,35 @@ public class Launcher {
         }
 
         new Client(hostName);
+    }
+
+    private static void wrapSystemOut() {
+        PrintStream origOut = System.out;
+        PrintStream interceptor = new Interceptor(origOut);
+        System.setOut(interceptor);
+    }
+
+    private static class Interceptor extends PrintStream {
+
+        static final String FINAL_RESULT = "Final result:";
+        static final Runnable runnable = () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            System.exit(0);
+        };
+
+        Interceptor(OutputStream out) {
+            super(out, true);
+        }
+
+        @Override
+        public void print(String s) {
+            if (s.trim().startsWith(FINAL_RESULT)) {
+                new Thread(runnable).start();
+            }
+            super.print(s);
+        }
     }
 }
